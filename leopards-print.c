ceb5918bad87734f08817fafb06e8ead9fc452de
Dithsy <92625525+Dithsy@users.noreply.github.com>
2022-04-19 11:46:00 +0300

    modifiers

--- leopards-print.c
+++ leopards-print.c
@@ -1,108 +1,84 @@
 #include "main.h"
 
+unsigned int print_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
+unsigned int print_string_width(buffer_t *output,
+		unsigned char flags, int wid, int prec, int size);
+unsigned int print_neg_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
+
 /**
- * print_bigS - Non printable characters
- * (0 < ASCII value < 32 or >= 127) are
- * printed this way: \x, followed by the ASCII code
- * value in hexadecimal (upper case - always 2 characters)
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Return: number of char printed
+ * print_width - Stores leading spaces to a buffer for a width modifier.
+ * @output: A buffer_t struct containing a character array.
+ * @printed: The current number of characters already printed to output
+ *           for a given number specifier.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ *
+ * Return: The number of bytes stored to the buffer.
  */
-int print_bigS(va_list l, flags_t *f)
+unsigned int print_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid)
 {
-	int i, count = 0;
-	char *res;
-	char *s = va_arg(l, char *);
+	unsigned int ret = 0;
+	char width = ' ';
 
-	(void)f;
-	if (!s)
-		return (_puts("(null)"));
-
-	for (i = 0; s[i]; i++)
+	if (NEG_FLAG == 0)
 	{
-		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
-		{
-			_puts("\\x");
-			count += 2;
-			res = convert(s[i], 16, 0);
-			if (!res[1])
-				count += _putchar('0');
-			count += _puts(res);
-		}
-		else
-			count += _putchar(s[i]);
-	}
-	return (count);
-}
-
-/**
- * print_rev - prints a string in reverse
- * @l: argument from _printf
- * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Return: length of the printed string
- */
-int print_rev(va_list l, flags_t *f)
-{
-	int i = 0, j;
-	char *s = va_arg(l, char *);
-
-	(void)f;
-	if (!s)
-		s = "(null)";
-
-	while (s[i])
-		i++;
-
-	for (j = i - 1; j >= 0; j--)
-		_putchar(s[j]);
-
-	return (i);
-}
-
-/**
- * print_rot13 - prints a string using rot13
- * @l: list of arguments from _printf
- * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Return: length of the printed string
- */
-int print_rot13(va_list l, flags_t *f)
-{
-	int i, j;
-	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	char *s = va_arg(l, char *);
-
-	(void)f;
-	for (j = 0; s[j]; j++)
-	{
-		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
-			_putchar(s[j]);
-		else
-		{
-			for (i = 0; i <= 52; i++)
-			{
-				if (s[j] == rot13[i])
-					_putchar(ROT13[i]);
-			}
-		}
+		for (wid -= printed; wid > 0;)
+			ret += _memcpy(output, &width, 1);
 	}
 
-	return (j);
+	return (ret);
 }
 
 /**
- * print_percent - prints a percent
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags in which we turn the flags on
- * Return: number of char printed
+ * print_string_width - Stores leading spaces to a buffer for a width modifier.
+ * @output: A buffer_t struct containing a character array.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ * @size: The size of the string.
+ *
+ * Return: The number of bytes stored to the buffer.
  */
-int print_percent(va_list l, flags_t *f)
+unsigned int print_string_width(buffer_t *output,
+		unsigned char flags, int wid, int prec, int size)
 {
-	(void)l;
-	(void)f;
-	return (_putchar('%'));
+	unsigned int ret = 0;
+	char width = ' ';
+
+	if (NEG_FLAG == 0)
+	{
+		wid -= (prec == -1) ? size : prec;
+		for (; wid > 0; wid--)
+			ret += _memcpy(output, &width, 1);
+	}
+
+	return (ret);
+}
+
+/**
+ * print_neg_width - Stores trailing spaces to a buffer for a '-' flag.
+ * @output: A buffer_t struct containing a character array.
+ * @printed: The current number of bytes already stored to output
+ *           for a given specifier.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int print_neg_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid)
+{
+	unsigned int ret = 0;
+	char width = ' ';
+
+	if (NEG_FLAG == 1)
+	{
+		for (wid -= printed; wid > 0; wid--)
+			ret += _memcpy(output, &width, 1);
+	}
+
+	return (ret);
 }
